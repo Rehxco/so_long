432479c47f1acd0c44e4e004788468585cafe1a6
Rehxco <salome.brchrd@gmail.com>
2025-10-20 20:58:25 +0200

    free

--- free/free_map.c
+++ free/free_map.c
@@ -6,12 +6,11 @@
 /*   By: sbrochar <sbrochar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/30 22:05:28 by sbrochar          #+#    #+#             */
-/*   Updated: 2025/10/20 18:23:53 by sbrochar         ###   ########.fr       */
+/*   Updated: 2025/10/20 20:57:51 by sbrochar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../so_long.h"
-
 
 void	free_map(char **map, int height)
 {
@@ -19,12 +18,12 @@ void	free_map(char **map, int height)
 
 	if (!map)
 		return ;
-	for (j = 0; j < height; j++)
+	j = 0;
+	while (j < height)
 	{
 		if (map[j])
-		{
 			free(map[j]);
-		}
+		j++;
 	}
 	free(map);
 }
